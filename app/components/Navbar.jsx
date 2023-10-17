43220a64336e3c9bc074d1a16758d955c1f829e5
User-XYZ <nwosu4686@gmail.com>
2023-10-17 19:09:17 +0100

    added tickets route & Navbar

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function Navbar() {
+  return (
+    <nav>
+        <h2>Tk Helpdesk</h2>
+        <Link href="/">Dashboard</Link>
+        <Link href="/tickets">Tickets</Link>
+    </nav>
+  )
+}
---
+++ app/tickets/page.jsx
@@ -0,0 +1,8 @@
+export default function Tickets() {
+
+    return(
+        <main>
+            <h1>Tickets</h1>
+        </main>
+    )
+}
