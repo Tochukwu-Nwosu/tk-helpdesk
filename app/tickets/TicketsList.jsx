f0dad03c21ac7d9e97a13f91de1cf2322904ca52
User-XYZ <nwosu4686@gmail.com>
2023-10-19 19:53:23 +0100

    modified tickets list

--- app/tickets/TicketsList.jsx
+++ app/tickets/TicketsList.jsx
@@ -1,10 +1,26 @@
 async function getTickets() {
-    // fetch data
+    const res = await fetch('http://localhost:4000/ticket', {
+        next: {
+            revalidate: 0         // stops next from using cache
+        }
+    })
+    const data = await res.json()
+
+    return data
 }
 
 export default async function TicketList() {
+    const tickets = await getTickets()
 
     return(
-        // create template with fetched data
+        <>
+            {tickets.map(ticket => (
+                <div key={ticket.id} className="card my-8">
+                    <h3>{ticket.title}</h3>
+                    <p>{ticket.body.slice(0, 200)}...</p>
+                    <div className={`pill ${ticket.priority}`}>{ticket.priority} priority</div>
+                </div>
+            ))}
+        </>
     )
 }
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,9 +1,12 @@
+import TicketList from "./TicketsList";
+
 export default function Tickets() {
 
     return(
         <main>
             <h2>Tickets</h2>
             {/* render TicketsList */}
+            <TicketList />
         </main>
     )
 }
