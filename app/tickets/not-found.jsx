809ae7116dbd4499177487a7bf77f499e1c091e6
User-XYZ <nwosu4686@gmail.com>
2023-10-21 20:53:26 +0100

    created custom 404 page

---
+++ app/tickets/not-found.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+function NotFound() {
+
+    return(
+        <main className="text-center">
+            <h2 className="text-3xl">Page Not found</h2>
+            <p>We could not find the page you were looking for.</p>
+            <p>Go back to all <Link href='/tickets'>tickets</Link></p>
+        </main>
+    )
+}
+
+export default NotFound
