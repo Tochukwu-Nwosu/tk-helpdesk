6c0e72bd2097c824ae006d06d17e3c647993a1b5
User-XYZ <nwosu4686@gmail.com>
2023-10-21 20:39:14 +0100

    updated dynamic segment

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,4 +1,16 @@
+import { notFound } from "next/navigation"
 
+export const dynamicParams = true
+
+// the generateStaticParams function lets next know the number of dynamic routes to pre-render
+export async function generateStaticParams() {
+    const res = await fetch('http://localhost:4000/ticket')
+    const data = await res.json()
+
+    return data.map(ticket => ({
+        id: ticket.id
+    }))
+}
 
 async function getTicket(id) {
     const res = await fetch('http://localhost:4000/ticket/' + id, {
@@ -7,6 +19,10 @@ async function getTicket(id) {
         }
     })
     const data = await res.json()
+
+    if(!res.ok) {
+        notFound()   // renders the not-found page if it's unable to make a fetch
+    }
     
     return data
 }
@@ -18,13 +34,13 @@ async function TicketDetail({ params }) {
         <main>
             <nav>
                 <div>
-                    <h2>Ticket</h2>
+                    <h2>Ticket Detail</h2>
                 </div>
             </nav>
 
             <div className="card">
                 <h3>{ticket.title}</h3>
-                <small>{ticket.email}</small>
+                <small>Created by {ticket.email}</small>
                 <p>{ticket.body}</p>
                 <div className={`pill ${ticket.priority}`}>{ticket.priority} priority</div>
             </div>
