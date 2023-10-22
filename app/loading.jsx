d57be58fb06b6c2342133f8f6296abd18249c5bf
User-XYZ <nwosu4686@gmail.com>
2023-10-22 21:06:50 +0100

    updated loading page

--- app/loading.jsx
+++ app/loading.jsx
@@ -3,7 +3,7 @@ export default function Loading() {
     return(
         <main classname="text-center">
             <h2 className="text-primary">Loading...</h2>
-            <p>Hopefully it will not take long :)</p>
+            <p>Hopefully it will not take long (:</p>
         </main>
     )
 }
