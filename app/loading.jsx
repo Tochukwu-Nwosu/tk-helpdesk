4aebbcf0417a99484ebd59a4839c06769699bf01
User-XYZ <nwosu4686@gmail.com>
2023-10-22 20:44:13 +0100

    created loading page

---
+++ app/loading.jsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+    
+    return(
+        <main classname="text-center">
+            <h2 className="text-primary">Loading...</h2>
+            <p>Hopefully it will not take long :)</p>
+        </main>
+    )
+}
