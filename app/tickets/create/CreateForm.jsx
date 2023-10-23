313cd9d9a75ed11d571fa46c9995c1ee83775614
User-XYZ <nwosu4686@gmail.com>
2023-10-23 16:41:51 +0100

    created form

--- app/tickets/create/CreateForm.jsx
+++ app/tickets/create/CreateForm.jsx
@@ -1,14 +1,51 @@
+"use client"
 
+import { useState } from "react"
 
 export default function CreateForm() {
-
+  const [ title, setTitle ] = useState('')
+  const [ body, setBody ] = useState('')
+  const [ priority, setPriority ] = useState('low')
+  const [ isLoading, setIsLoading ] = useState(false)
 
   return (
-    <main className="w-1/2">
-      <form>
-
+    <main>
+      <form className="w-1/2">
+        <label>
+            Title:
+            <input 
+                type="text"
+                required
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+             />
+        </label>
+        <label>
+            Body:
+            <textarea 
+                type="text"
+                required
+                onChange={(e) => setBody(e.target.value)}
+                value={body}
+             />
+        </label>
+        <label>
+            Priority:
+            <select
+                required
+                onChange={(e) => setPriority(e.target.value)}
+                value={priority}
+            >
+                <option value="low">low priority</option>
+                <option value="medium">medium priority</option>
+                <option value="high">high priority</option>
+            </select>
+        </label>
+        <button className="btn-primary mt-8 mb-4">
+            {isLoading && <p>Adding...</p>}
+            {!isLoading && <p>Add ticket</p>}
+        </button>
       </form>
-      <h3>Form</h3>
     </main>
   )
 }
