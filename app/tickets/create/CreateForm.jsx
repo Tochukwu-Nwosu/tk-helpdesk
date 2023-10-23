550544a08952401e9eff03b3ce805cf134b15245
User-XYZ <nwosu4686@gmail.com>
2023-10-23 17:27:24 +0100

    added redirection after submitting form

--- app/tickets/create/CreateForm.jsx
+++ app/tickets/create/CreateForm.jsx
@@ -1,16 +1,40 @@
 "use client"
 
+import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 export default function CreateForm() {
+  const router = useRouter()
   const [ title, setTitle ] = useState('')
   const [ body, setBody ] = useState('')
   const [ priority, setPriority ] = useState('low')
   const [ isLoading, setIsLoading ] = useState(false)
 
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    setIsLoading(true)
+
+    const ticket = {
+        title, body, priority, email: "unknown@tk.dev"
+    }
+
+    // Posts the collected data
+    await fetch('http://localhost:4000/ticket', {
+        method: 'POST',
+        body: JSON.stringify(ticket),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+
+    // redirects to tickets page
+    router.push('/tickets')  
+  }
+
   return (
     <main>
-      <form className="w-1/2">
+      <form onSubmit={handleSubmit} className="w-1/2">
         <label>
             Title:
             <input 
@@ -41,7 +65,7 @@ export default function CreateForm() {
                 <option value="high">high priority</option>
             </select>
         </label>
-        <button className="btn-primary mt-8 mb-4">
+        <button className="btn-primary mt-8 mb-4" disabled={isLoading}>
             {isLoading && <p>Adding...</p>}
             {!isLoading && <p>Add ticket</p>}
         </button>
