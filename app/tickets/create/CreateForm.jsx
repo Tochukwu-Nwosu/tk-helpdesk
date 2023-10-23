c915fa6dbfc457a0cdee9cfc76b843b926b4befa
User-XYZ <nwosu4686@gmail.com>
2023-10-23 15:35:59 +0100

    created create page

---
+++ app/tickets/create/CreateForm.jsx
@@ -0,0 +1,14 @@
+
+
+export default function CreateForm() {
+
+
+  return (
+    <main className="w-1/2">
+      <form>
+
+      </form>
+      <h3>Form</h3>
+    </main>
+  )
+}
---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,13 @@
+import CreateForm from "./CreateForm"
+
+function CreateTicket() {
+
+    return(
+        <main>
+            <h2 className="text-3xl text-center">Create a Ticket</h2>
+            <CreateForm />
+        </main>
+    )
+}
+
+export default CreateTicket
